fa2ab7c1ce5d53f1178a0a7642affc8056259102
LuisR27 <luisesteban.ramirez@ucr.ac.cr>
2020-11-02 14:10:05 -0600

    Agregué pruebas para detalles de artículo

--- Iteracion_2/PruebasComunidadPractica/Articulos/UnitTestArticulos.cs
+++ Iteracion_2/PruebasComunidadPractica/Articulos/UnitTestArticulos.cs
@@ -19,5 +19,63 @@ namespace PruebasComunidadPractica.Articulos
 
             Assert.IsFalse  (articulos.IsNullOrEmpty(), "No se retornaron artículos");
         }
+
+        [TestMethod]
+        public void ValidarRetornoInformacionArticulo()
+        {
+            ArticuloModel articuloModel;
+            articuloModel = new ArticuloModel();
+            string ID = "";
+            List<List<string>> articulos = articuloModel.validarRetornoArticulos();
+            if (!articulos.IsNullOrEmpty())
+            {
+                ID = articulos[0][0];
+            }
+            else
+            {
+                Assert.Fail();
+            }
+            string [] InformacionArticulo = articuloModel.RetornarDatos(ID);
+
+            Assert.IsFalse(InformacionArticulo.IsNullOrEmpty(), "No se retornaron detalles del artículo");
+        }
+        [TestMethod]
+        public void ValidarRetornoAutorArticulo()
+        {
+            ArticuloModel articuloModel;
+            articuloModel = new ArticuloModel();
+            string ID = "";
+            List<List<string>> articulos = articuloModel.validarRetornoArticulos();
+            if (!articulos.IsNullOrEmpty())
+            {
+                ID = articulos[0][0];
+            }
+            else
+            {
+                Assert.Fail();
+            }
+            List<string> autores = articuloModel.RetornarAutor(ID);
+
+            Assert.IsFalse(autores.IsNullOrEmpty(), "No se retornaron autores del artículo");
+        }
+        [TestMethod]
+        public void ValidarRetornoTopicosArticulo()
+        {
+            ArticuloModel articuloModel;
+            articuloModel = new ArticuloModel();
+            string ID = "";
+            List<List<string>> articulos = articuloModel.validarRetornoArticulos();
+            if (!articulos.IsNullOrEmpty())
+            {
+                ID = articulos[0][0];
+            }
+            else
+            {
+                Assert.Fail();
+            }
+            List<string> topicos = articuloModel.RetornarTopicos(ID);
+
+            Assert.IsFalse(topicos.IsNullOrEmpty(), "No se retornaron tópicos del artículo");
+        }
     }
 }
